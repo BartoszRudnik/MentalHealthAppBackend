caaed8fb4b1e7208ffe5cb90692cee8eefe769ae
BartoszRudnik <Bartosz_Rudnik@outlook.com>
2025-10-22 15:59:34 +0200

    (feat) add GetAdviceByMoodUseCase

---
+++ application/use-cases/GetAdviceByMoodUseCase.js
@@ -0,0 +1,17 @@
+const Meditation = require("../../domain/entities/meditation");
+const UseCaseInterface = require("../interfaces/UseCaseInterface");
+
+class GetAdviceByMoodUseCase extends UseCaseInterface{
+    constructor(quotesRepository){
+        super();
+        this.quotesRepository = quotesRepository;
+    }
+
+    async execute(mood){
+        const quotesData = await this.quotesRepository.getAdviceByMood(mood);
+
+        return new Meditation({text: quotesData});
+    }
+}
+
+module.exports = GetAdviceByMoodUseCase;
