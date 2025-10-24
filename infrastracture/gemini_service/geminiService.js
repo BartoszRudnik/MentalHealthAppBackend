67badc26c779037a977d09e38f01fedf9312d78b
BartoszRudnik <Bartosz_Rudnik@outlook.com>
2025-10-24 16:00:31 +0200

    (feat) add GeminiService

---
+++ infrastracture/gemini_service/geminiService.js
@@ -0,0 +1,29 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const QuotesRepository = require("../../application/interfaces/QuotesRepositoryInterface");
+
+const geminiAi = new GoogleGenerativeAI("API_KEY");
+const model = geminiAi.getGenerativeModel({model: "gemini-1.5-pro"});
+
+class GeminiApi extends QuotesRepository{
+    async getAdviceByMood(mood){
+        const prompt = `${mood}`;
+
+        const result = await model.generateContent(prompt);
+
+        const response = await result.response;
+        const text = response.text();
+
+        return text;
+    }
+
+    async getDailyQuotes(){
+        const prompt = '';
+
+        const result = await model.generateContent(prompt);
+
+        const response = await result.response;
+        const text = response.text();
+
+        return text;
+    }
+}
